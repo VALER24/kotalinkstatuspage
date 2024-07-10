057144fddeebd8b0b6dbf9dd9ef3caae76428273
29henbar <99993697+29henbar@users.noreply.github.com>
2024-07-10 08:47:01 -0500

    Update status.js

--- api/status.js
+++ api/status.js
@@ -3,6 +3,9 @@ module.exports = (req, res) => {
     const status = {
         allstar: 'online',
         echolink: 'offline',
+        ab_mb: 'online',
+        mb: 'online',
+        usrp2p25: 'offline',
         dmr: 'online',
         ysf: 'online',
         p25: 'offline',
